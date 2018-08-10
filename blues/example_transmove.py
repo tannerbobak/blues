e7645573a2c654360142d4edbc2063350e64c134
Tanner Bobak <tanner.bobak@colorado.edu>
2018-08-10 17:48:34 -0600

    fix import for translation move

--- blues/example_transmove.py
+++ blues/example_transmove.py
@@ -1,4 +1,4 @@
-from blues.moves import RandomLigandTranslationMove
+from translation_move import RandomLigandTranslationMove
 from blues.engine import MoveEngine
 from blues.simulation import *
 import json
